502cfefc0dad0a34d59d3cc1cbb929dc8f92ee2f
HunterHarvell <hunterharvell95@gmail.com>
2023-03-20 19:13:45 -0500

    sets up schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,7 @@
+const { AuthenticationError } = require('apollo-server-experess');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    
+}
