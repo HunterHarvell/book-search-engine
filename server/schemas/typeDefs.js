b05ae88fa881640aa70c84d7f80b95caea8e3b99
HunterHarvell <hunterharvell95@gmail.com>
2023-03-20 19:25:22 -0500

    adds resolvers and type defs for login and creating a user

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+
+
+
+
+
+
+
+`;
