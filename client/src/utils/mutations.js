3597ab03a110e2e103c971cb0c9eeae4a82d7e0e
HunterHarvell <hunterharvell95@gmail.com>
2023-03-30 22:02:19 -0500

    adds user mutations and save book mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client'
+
+export const LOGIN_USER = gql `
+    mutation login($email: String, $password: String) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`
+
+export const ADD_USER = gql `
+    mutation addUser($username: String, $email: String, $password: String) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`
+
+export const SAVE_BOOK = gql `
+    mutation saveBook($BookData: BookInput) {
+        saveBook(BookData: $BookData) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                title
+                authors
+                image
+                description
+                link
+            }
+        }
+    }
+`
