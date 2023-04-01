16bcf3f9df93b7767cac33016d26461167784cf5
HunterHarvell <hunterharvell95@gmail.com>
2023-03-31 21:26:58 -0500

    add express to server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const express = require('express');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
